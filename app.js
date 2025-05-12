aa8875b5061e1557f3cb6ada2f565fc8b3d1af5f
abaspnr <117168519+abaspnr@users.noreply.github.com>
2025-05-13 01:49:48 +0300

    redesing

--- app.js
+++ app.js
@@ -1,95 +1,30 @@
-// Firebase Ayarları
-const firebaseConfig = {
-  apiKey: "AIzaSyCfjN1tbMatLamGZNqRZZcdvoM8Vbx0RlM",
-  authDomain: "message-app-e45fa.firebaseapp.com",
-  databaseURL: "https://message-app-e45fa-default-rtdb.firebaseio.com",
-  projectId: "message-app-e45fa",
-  storageBucket: "message-app-e45fa.appspot.com",
-  messagingSenderId: "1090017668550",
-  appId: "1:1090017668550:web:e5f1a12735a3315648d6c7"
-};
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.database();
-
-function showLogin() {
-  document.getElementById('app').innerHTML = `
-    <h2>Giriş Yap</h2>
-    <input type="email" id="loginEmail" placeholder="E-posta"><br>
-    <input type="password" id="loginPassword" placeholder="Şifre"><br>
-    <button onclick="login()">Giriş Yap</button><br>
-    <a href="#" onclick="showRegister()">Hesabınız yok mu? Kayıt Ol</a><br>
-    <a href="#" onclick="showReset()">Şifremi Unuttum</a>
-  `;
+function navigate(url) {
+  window.location.href = url;
 }
 
-function showRegister() {
-  document.getElementById('app').innerHTML = `
-    <h2>Kayıt Ol</h2>
-    <input type="email" id="registerEmail" placeholder="E-posta"><br>
-    <input type="password" id="registerPassword" placeholder="Şifre"><br>
-    <button onclick="register()">Kayıt Ol</button><br>
-    <a href="#" onclick="showLogin()">Zaten hesabınız var mı? Giriş Yap</a>
-  `;
-}
-
-function showReset() {
-  document.getElementById('app').innerHTML = `
-    <h2>Şifre Sıfırlama</h2>
-    <input type="email" id="resetEmail" placeholder="E-posta"><br>
-    <button onclick="resetPassword()">Sıfırlama Maili Gönder</button><br>
-    <a href="#" onclick="showLogin()">Girişe Dön</a>
-  `;
-}
-
-function login() {
-  const email = document.getElementById('loginEmail').value;
-  const password = document.getElementById('loginPassword').value;
-
-  auth.signInWithEmailAndPassword(email, password)
-    .then(userCredential => {
-      const user = userCredential.user;
-      showChat(user);
-    })
-    .catch(error => alert(error.message));
+function handleAuthError(error) {
+  alert(error.message);
 }
 
 function register() {
-  const email = document.getElementById('registerEmail').value;
-  const password = document.getElementById('registerPassword').value;
-
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
   auth.createUserWithEmailAndPassword(email, password)
-    .then(userCredential => {
-      const user = userCredential.user;
-      db.ref('users/' + user.uid).set({
-        email: user.email,
-        uid: user.uid
-      });
-      showChat(user);
-    })
-    .catch(error => alert(error.message));
+    .then(() => navigate('chat.html'))
+    .catch(handleAuthError);
+}
+
+function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => navigate('chat.html'))
+    .catch(handleAuthError);
 }
 
 function resetPassword() {
-  const email = document.getElementById('resetEmail').value;
+  const email = document.getElementById('email').value;
   auth.sendPasswordResetEmail(email)
-    .then(() => alert('Şifre sıfırlama maili gönderildi.'))
-    .catch(error => alert(error.message));
+    .then(() => alert('Şifre sıfırlama maili gönderildi'))
+    .catch(handleAuthError);
 }
-
-function showChat(user) {
-  document.getElementById('app').innerHTML = `
-    <h2>Hoşgeldiniz, ${user.email}</h2>
-    <button onclick="auth.signOut().then(showLogin)">Çıkış Yap</button>
-  `;
-}
-
-// Oturum Açık mı Kontrol Et
-auth.onAuthStateChanged(user => {
-  if (user) {
-    showChat(user);
-  } else {
-    showLogin();
-  }
-});
---
+++ firebse.js
@@ -0,0 +1,13 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCfjN1tbMatLamGZNqRZZcdvoM8Vbx0RlM",
+  authDomain: "message-app-e45fa.firebaseapp.com",
+  databaseURL: "https://message-app-e45fa-default-rtdb.firebaseio.com",
+  projectId: "message-app-e45fa",
+  storageBucket: "message-app-e45fa.appspot.com",
+  messagingSenderId: "1090017668550",
+  appId: "1:1090017668550:web:e5f1a12735a3315648d6c7"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
