da66118ca2701ebf9cc69d4bd25b056d051938d3
abaspnr <117168519+abaspnr@users.noreply.github.com>
2025-05-13 11:25:07 +0300

    a

--- firebse.js
+++ firebse.js
@@ -1,4 +1,3 @@
-// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyCfjN1tbMatLamGZNqRZZcdvoM8Vbx0RlM",
   authDomain: "message-app-e45fa.firebaseapp.com",
@@ -8,6 +7,7 @@ const firebaseConfig = {
   messagingSenderId: "1090017668550",
   appId: "1:1090017668550:web:e5f1a12735a3315648d6c7"
 };
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
