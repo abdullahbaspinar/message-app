928d953788260d8757b2119fc9fc846d4b5669ca
abaspnr <117168519+abaspnr@users.noreply.github.com>
2025-05-28 00:10:41 +0300

    redesing

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,26 @@
+        const firebaseConfig = {
+      apiKey: "AIzaSyCfjN1tbMatLamGZNqRZZcdvoM8Vbx0RlM",
+      authDomain: "message-app-e45fa.firebaseapp.com",
+      databaseURL: "https://message-app-e45fa-default-rtdb.firebaseio.com",
+      projectId: "message-app-e45fa",
+      storageBucket: "message-app-e45fa.appspot.com",
+      messagingSenderId: "1090017668550",
+      appId: "1:1090017668550:web:e5f1a12735a3315648d6c7"
+    };
+    firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
+    const db = firebase.database();
+  function setDynamicVh() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  window.addEventListener('resize', setDynamicVh);
+  window.addEventListener('orientationchange', setDynamicVh);
+  window.addEventListener('load', setDynamicVh);
+  setDynamicVh();
 
+
+  
 
     const logoutBtn = document.getElementById('logoutBtn');
     const userListContainer = document.getElementById('userListContainer');
